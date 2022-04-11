5e3d3aa39886eff1b401eb48675d7d7ca5f121b4
Tomás Elizondo <tomaselizondo@piper.local>
2022-04-10 23:19:48 -0300

    I got stuck with .mjs

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom'
+import Web3 from "web3/dist/web3.min";
 import Home from './views/home' 
 
 function App() {
   useEffect(() => {
     if (window.ethereum) {
-      window.ethereum.request({ method: 'eth_requestAccounts'}).then(accounts => console.log(accounts));
+      // window.ethereum.request({ method: 'eth_requestAccounts'})
+      // .then(accounts => console.log(accounts));
+      const web3 = new Web3(window.ethereum);
+      web3.eth.requestAccounts().then(console.log); 
     }
-
   }, []);
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Home/>} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
     </>
   );
 }
